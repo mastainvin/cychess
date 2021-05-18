fbad387829c2e64c107acc461d7ff3845acce654
Migywara <raroumougom@gmail.com>
2021-05-18 13:47:46 +0400

    page de profil

--- client/src/components/Profil/UpdateProfil.js
+++ client/src/components/Profil/UpdateProfil.js
@@ -1,9 +1,33 @@
-import React from "react";
-import { useSelector } from "react-redux";
-// import UploadImg from "./UploadImg";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateBio, updateDateDeNaissance, updateGenre } from "../../actions/user.actions";
+import UploadImg from "./UploadImg";
 
 const Updateprofil = () => {
+    const [bio, setBio] = useState(userData.bio);
+    const [dateDeNaissance, setDateDeNaissance] = useState(userData.bio);
+    const [genre, setGenre] = useState('');
+    const [updateForm, setUpdateForm] = useState(false);
+    const [updateBorn, setUpdateBorn] = useState(false);
+    const [updateSex, setUpdateSex] = useState(false);
     const userData = useSelector((state) => state.userReducer);
+    const dispatch = useDispatch();
+
+    const handleUpdate = () => {
+        dispatch(updateBio(userData._id, bio));
+        setUpdateForm(false);
+        
+    }
+
+    const handleUpdateBorn = () => {
+        dispatch(updateDateDeNaissance(userData._id, dateDeNaissance));
+        setUpdateBorn(false);
+    }
+
+    const handleUpdateSex = () => {
+        dispatch(updateGenre._id, genre);
+        setUpdateSex(false);
+    }
 
     return (
         <div className="profil-container">
@@ -11,9 +35,62 @@ const Updateprofil = () => {
             <div className="update-container">
                 <div className="left-part">
                     <h4>Photo de profil</h4>
-                    authmiddleware changer regarder a 2:56:30
                     <img src={userData.userProfil} alt="user-pic" />
-                    {/* <UploadImg /> */}
+                    <UploadImg />
+                </div>
+                <div className="right-part">
+                    <h3>Bio</h3>
+                    {updateForm === false && (
+                        <>
+                            <p onClick={() => setUpdateForm(!updateForm)}>{userData.bio}</p>
+                            <button onClick={() => setUpdateForm(!updateForm)}>Modifier bio</button>
+                        </>
+                    )}
+                    {updateForm && (
+                        <>
+                            <textarea
+                                type="text"
+                                defaultValue={userData.bio} 
+                                onChange={(e) => setBio(e.target.value)}
+                            ></textarea>
+                            <button onClick={handleUpdate}>Valider modifications</button>
+                        </>
+                    )}
+                    <h3>Informations supplémentaires</h3>
+                    {updateBorn === false && (
+                        <>
+                            <p onClick={() => setUpdateBorn(!updateBorn)}> Date de naissance :{userData.dateDeNaissance}</p>
+                            <button onClick={() => setUpdateBorn(!updateBorn)}>Modifier date de naissance</button>
+                        </>
+                    )}
+                    {updateBorn && (
+                        <>
+                            <input
+                                type="date" 
+                                name="born-date"
+                                defaultValue={userData.dateDeNaissance}
+                                onChange={(e) => setDateDeNaissance(e.target.value)}
+                            />
+                            <button onClick={handleUpdateBorn}>Valider modifications</button>
+                        </>
+                    )}
+                    {/* <p>Genre </p>
+                    {updateSex === false && (
+                        <>
+                            <p onClick={() => setUpdateSex(!updateSex)}>{userData.born}</p>
+                            <button onClick={() => setUpdateSex(!updateSex)}>Modifier Genre</button>
+                        </>
+                    )}
+                    {updateSex && (
+                        <>
+                            <input
+                                type="text"
+                                defaultValue={userData.genre} 
+                                onChange={(e) => setGenre(e.target.value)}
+                            ></input>
+                            <button onClick={handleUpdate}>Valider modifications</button>
+                        </>
+                    )} */}
                 </div>
             </div>
         </div>
--- client/src/components/Profil/UploadImg.js
+++ client/src/components/Profil/UploadImg.js
@@ -1,6 +1,5 @@
-import e from "express";
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { uploadPicture } from "../../actions/user.actions";
 
 const UploadImg = () => {
@@ -9,25 +8,27 @@ const UploadImg = () => {
     const userData = useSelector((state) => state.userReducer);
 
     const handlePicture = () => {
-        e.preventDefault();
         const data = new FormData();
         data.append("name", userData.pseudonyme);
         data.append("userId", userData._id);
         data.append("file", file);
+
         dispatch(uploadPicture(data, userData._id));
-    };
+
+    }
 
     return (
-        <form action="" onSubmit={handlePicture} className="upload-pic">
+        <form action ="" onSubmit={handlePicture} className="upload-pic">
             <label htmlFor="file">Changer d'image</label>
-            <input
-                type="file"
-                id="file"
-                accept=".jpg, .jpeg, .png"
-                onChange={(e) => setFile(e.target.files[0])}
+            <input 
+            type="file" 
+            id="file" 
+            name="file" 
+            accept accept=".jpg, .jpeg, .png"
+            onChange={(e) => setFile(e.target.files[0])}
             ></input>
             <br />
-            <input type="submit" vlue="Envoyer"></input>
+            <input type="submit" value="Envoyer"></input>
         </form>
     );
 };
