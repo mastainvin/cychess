e604d6992ffdd2891cb54ed6060a5b7b54959eb7
Vincent Mastain <vincent.mastain@outlook.fr>
2021-05-21 13:11:46 +0200

    correction bug redirection page principale

--- client/src/pages/AdminEvents.js
+++ client/src/pages/AdminEvents.js
@@ -4,9 +4,8 @@ import { isAdmin, isEmpty } from "../components/Utils";
 import { Redirect } from "react-router";
 import Header from "../components/header";
 import { getEvents } from "../actions/event.actions";
-import { Button, ListGroup, ListGroupItem } from "reactstrap";
+import { Button, ListGroup, ListGroupItem, Spinner } from "reactstrap";
 import "./event-admin.scss";
-
 import EventModal from "../components/Event-admin/EventModal";
 import ListElement from "../components/Event-admin/ListElement";
 
@@ -16,7 +15,8 @@ const AdminEvent = () => {
     const events = useSelector((state) => state.eventReducer);
     const [loadEvent, setLoadEvent] = useState(true);
     const dispatch = useDispatch();
-
+    const [isUserAdmin, setIsUserAdmin] = useState(userData.admin);
+    const [inLoad, setInLoad] = useState(true);
     const [modal, setModal] = useState(false);
 
     const toggle = () => {
@@ -28,30 +28,46 @@ const AdminEvent = () => {
             dispatch(getEvents());
             setLoadEvent(false);
         }
-    }, [loadEvent, dispatch, events]);
+        if (inLoad && !isEmpty(userData)) {
+            setIsUserAdmin(userData.admin);
+            setInLoad(false);
+        }
+    }, [userData, loadEvent, dispatch, events]);
 
     return (
         <div className="container">
             <EventModal toggle={toggle} modal={modal} />
             <Header title="Administration - Evénements" />
-            {notAdmin ? (
-                <Redirect to="/" />
+            {inLoad ? (
+                <Spinner color="success" />
             ) : (
-                <div>
-                    <ListGroup className="event-list">
-                        <ListGroupItem>
-                            <div className="list-row">
-                                <Button color="success" onClick={toggle}>
-                                    Ajouter un événement !
-                                </Button>
-                            </div>
-                        </ListGroupItem>
-                        {!isEmpty(events) &&
-                            events.map((event) => (
-                                <ListElement event={event} key={event._id} />
-                            ))}
-                    </ListGroup>
-                </div>
+                <>
+                    {!isUserAdmin ? (
+                        <Redirect to="/" />
+                    ) : (
+                        <div>
+                            <ListGroup className="event-list">
+                                <ListGroupItem>
+                                    <div className="list-row">
+                                        <Button
+                                            color="success"
+                                            onClick={toggle}
+                                        >
+                                            Ajouter un événement !
+                                        </Button>
+                                    </div>
+                                </ListGroupItem>
+                                {!isEmpty(events) &&
+                                    events.map((event) => (
+                                        <ListElement
+                                            event={event}
+                                            key={event._id}
+                                        />
+                                    ))}
+                            </ListGroup>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     );
