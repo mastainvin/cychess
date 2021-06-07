21754823f29316a5116588223953c5a3bae923c1
Vincent Mastain <vincent.mastain@outlook.fr>
2021-06-07 17:06:56 +0200

    correctif 12

--- server.js
+++ server.js
@@ -40,17 +40,17 @@ app.use("/api/post", postRoutes);
 app.use("/api/recette", recetteRoutes);
 app.use("/api/event", eventRoutes);
 
-
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
     // Set static folder
-    app.use(express.static('client/build'))
+    app.use(express.static("client/build"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 //server
+const port = process.env.PORT || 5000;
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
+    console.log(`Listening on port ${port}`);
 });
