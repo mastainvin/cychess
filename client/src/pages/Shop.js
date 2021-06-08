6a6b5add92a2e03e0e3bb4e7d7b6ace7ee76d3ac
Vincent Mastain <vincent.mastain@outlook.fr>
2021-06-08 15:46:31 +0200

    correction

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -91,6 +91,7 @@ function Shop() {
                                     <CardImg
                                         top
                                         width="100%"
+                                        height="50%"
                                         src={produit.productProfil}
                                         alt="Card image cap"
                                     />
